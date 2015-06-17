1ea141f486d89c25566e2a80d60e70e877d8c0d7
tridoof <sam445544@gmail.com>
2015-06-17 01:54:56 -0700

    initial commit

---
+++ app/src/main/java/com/samuel/destructo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.samuel.destructo;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Go to log in activity
+        Intent intent = new Intent(this, LoginActivity.class);
+        //A flag is needed to skip the MainAcitivty from showing up?
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //Clear the task so that we cant back arrow into it
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
